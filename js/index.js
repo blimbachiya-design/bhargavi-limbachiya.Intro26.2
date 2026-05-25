48e7e405118d51fb6628054aaff618250b276d12
blimbachiya-design <266343831+blimbachiya-design@users.noreply.github.com>
2026-05-25 19:55:57 -0400

    fixed lesson 7 feedback

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 const footer = document.createElement('footer');
-    document.body.appendChild(footer);
+document.body.appendChild(footer);
 
 const today = new Date();
 const thisYear = today.getFullYear();
+const footerElement = document.querySelector('footer');
 const copyright = document.createElement('p');
     copyright.innerHTML = `Bhargavi Limbachiya. All rights reserved. © ${thisYear} `;
-        footer.appendChild(copyright);
+        footerElement.appendChild(copyright);
 
 const skills = ['HTML', 'CSS', 'JavaScript', 'GitHub', 'VS Code'];
-const skillsSection = document.getElementById('Skills');
+const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
